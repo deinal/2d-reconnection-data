afe5b74e8afa9aca990c873ce65893ae1deb67fd
Daniel Holmberg <daniel.holmberg97@gmail.com>
2024-05-12 10:29:04 +0300

    Fix loader (#3)
    * Your commit message here
    
    * Your commit message here
    
    * Update loader
    
    * Update loader
    
    * Update loader
    
    * Debugged and properly tested extraction-interpolation. Slightly modified visualisation script. BGD is added. Style corrected as well. Thanks for showing pep8. Recent version contains two time-steps for each run as the test. Select your time intervals.
    
    * Defined time intervals in constants
    
    * Extract new set
    
    * Remove unintended artefacts from merge
    
    ---------
    
    Co-authored-by: Zaitsev Ivan <ivanzait@turso01.cs.helsinki.fi>

--- constants.py
+++ constants.py
@@ -1,29 +1,28 @@
 
 runs = [
     {
-        "id": "BGD",
-        "t_min": 550,
-        "t_max": 990,
-    },
-    {
         "id": "BCH",
-        "t_min": 1800,
+        "t_min": 1801,
         "t_max": 4300,
     }, 
     {
         "id": "BCQ",
-        "t_min": 1075,
+        "t_min": 1076,
         "t_max": 2875,
     },
     {
+        "id": "BGD",
+        "t_min": 590,
+        "t_max": 989,
+    },
+    {
         "id": "BGF",
-        "t_min": 230,
+        "t_min": 278,
         "t_max": 477,
     }
+]
 
-    ]
-
-Re = 6371000  # Earth's radius
+RE = 6371000  # Earth's radius
 
 xmin = -20
 xmax = 10
